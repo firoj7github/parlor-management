aba345cba38118193e264db8b69877107b8f486a
firoj7github <fkfiroj02@gmail.com>
2024-03-04 13:13:22 +0600

    config

--- config/app.php
+++ config/app.php
@@ -4,16 +4,7 @@ use Illuminate\Support\Facades\Facade;
 
 return [
 
-    /*
-    |--------------------------------------------------------------------------
-    | Application Name
-    |--------------------------------------------------------------------------
-    |
-    | This value is the name of your application. This value is used when the
-    | framework needs to place the application's name in a notification or
-    | any other location as required by the application or its packages.
-    |
-    */
+
 
     'name' => env('APP_NAME', 'Laravel'),
 
@@ -211,7 +202,7 @@ return [
         // Get Client Browser Information
         Jenssegers\Agent\AgentServiceProvider::class,
         Unicodeveloper\Paystack\PaystackServiceProvider::class,
-        
+
 
         // Import/Export Excel
         Maatwebsite\Excel\ExcelServiceProvider::class,
